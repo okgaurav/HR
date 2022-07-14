0438c53c250ae6b9a7e497688989a238860494fb
GUPTA Ritik <ritik.gupta@soprabanking.com>
2022-07-14 15:33:48 +0530

    Landing page design updated

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,6 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  signIn = true;
+  signUp = false;
+
+  toggleState() : void{
+    this.signIn = !this.signIn;
+    this.signUp = !this.signUp;
+  }
 
   constructor() { }
 
